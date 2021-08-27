6766ae26081898a7e5817d8a4ad114192e189817
Nemanja <nemanja.mijailovic2912@gmail.com>
2021-08-27 06:00:51 +0200

    jbmt zivot

---
+++ src/styles/global.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { StyleSheet } from "react-native";
+
+const global = StyleSheet.create({
+  screen: {
+    flex: 1,
+  },
+  text: {
+    fontSize: 50,
+  },
+});
+
+export default global;
